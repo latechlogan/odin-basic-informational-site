caacc6d891e15c3446675f76b74f1edb1eb412bc
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-21 22:40:58 -0600

    feat: add fs write to display views with server

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require("node:http");
-// const fs = require("node:fs");
+const fs = require("node:fs");
 
 const hostname = "localhost";
 const port = "8080";
 
 const server = http.createServer((req, res) => {
-  //
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server listening at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
