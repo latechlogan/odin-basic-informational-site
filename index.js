12e8e9b5c5bfbd254f3a6ee4992383f502d7d309
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-12-21 22:43:08 -0600

    feat: add routing for views, including 404

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ const port = "8080";
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
 
-  fs.readFile("./views/index.html", (err, data) => {
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
